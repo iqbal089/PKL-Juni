2e49d0ccdb676743f029f761bb4d7ffe7316c0ed
iqbal089 <51896973+iqbal089@users.noreply.github.com>
2019-06-17 11:25:12 +0700

    a
    coba

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pendaftaran | CV Djuara</title>
+</head>
+
+<body>
+    <header>
+        <h1>CV Djuara</h1>
+    </header>
+    <h4>PKL</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">Daftar Pelanggan</a></li>
+            <li><a href="list-buyer.php">Kode Pelanggan</a></li>
+        </ul>
+    </nav>
+
+    </body>
+</html>
